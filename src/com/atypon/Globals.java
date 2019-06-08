410cdc02e65d8c1041c5540bedecc9b5d055be7a
Asa Abbad <asa0abbad@gmail.com>
2019-06-08 14:23:06 +0300

    Push Globals.java

---
+++ src/com/atypon/Globals.java
@@ -0,0 +1,10 @@
+package com.atypon;
+
+public class Globals {
+    public static final String INPUT_FILE_NAME = "test.txt";
+    public static final String[] SPLITTERS = new String[] { ":", "-", ".", "***" };
+
+    // Server Communication
+    public static final String ACK_MSG = "ACK";
+    public static final String END_MSG = "END";
+}
