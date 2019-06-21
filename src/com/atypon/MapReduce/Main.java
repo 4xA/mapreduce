adfeeaa76743c979efadd540a67d5e0f2c42d58b
Asa Abbad <asa0abbad@gmail.com>
2019-06-21 22:07:31 +0300

    Include important file changes

--- src/com/atypon/MapReduce/Main.java
+++ src/com/atypon/MapReduce/Main.java
@@ -10,10 +10,11 @@ public class Main {
 
         config.setInputMethod(JobConfig.FILE);
         config.setFileName(Globals.INPUT_FILE_NAME);
-        config.setSplitters(Globals.SPLITTERS);
-//        config.setFileName("input.txt");
-//        config.setSplitters(String.format("%n"));
-        config.setMapNodesCount(10);
+        config.setSplitterRegex("[a-zA-Z]+");
+        config.setMapNodesCount(2);
+        config.setReduceNodesCount(7);
+        config.setMapServerPort(6000);
+        config.setReduceServerPort(10000);
 
         Job job = new Job(config);
         job.start();
--- src/com/atypon/MapReduce/util/Splitter.java
+++ src/com/atypon/MapReduce/util/Splitter.java
@@ -2,18 +2,23 @@ package com.atypon.MapReduce.util;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Splitter {
+    public static final String WORD = "word";
+
     private Splitter() {}
 
     public static ArrayList<String> split(String string, String ...splitters) {
         if (splitters.length == 0) return null;
 
         String regex = "";
+        String[] array;
 
         // Make splitters valid regex
         for (int i = 0; i < splitters.length; i++) {
-            if (splitters[i].contains("\\"))splitters[i] =  splitters[i].replace("\\", "\\\\");
+            if (splitters[i].contains("\\")) splitters[i] = splitters[i].replace("\\", "\\\\");
             if (splitters[i].contains(".")) splitters[i] = splitters[i].replace(".", "\\.");
             if (splitters[i].contains("^")) splitters[i] = splitters[i].replace("^", "\\^");
             if (splitters[i].contains("$")) splitters[i] = splitters[i].replace("$", "\\$");
@@ -27,13 +32,27 @@ public class Splitter {
             if (splitters[i].contains("|")) splitters[i] = splitters[i].replace("|", "\\|");
             if (splitters[i].contains("-")) splitters[i] = splitters[i].replace("-", "\\-");
 
-            if (i < splitters.length-1)
+            if (i < splitters.length - 1)
                 regex += splitters[i] + "|";
         }
-        regex += splitters[splitters.length-1];
+        regex += splitters[splitters.length - 1];
 
-        String[] array = string.split(regex);
+        array = string.split(regex);
 
         return new ArrayList<String>(Arrays.asList(array));
+    }
+
+    public static ArrayList<String> split(String string, String regex) {
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(string);
+
+            ArrayList<String> list = new ArrayList<>();
+
+            while(matcher.find()) {
+                String word = matcher.group();
+                list.add(word);
+            }
+
+            return list;
     }
 }
